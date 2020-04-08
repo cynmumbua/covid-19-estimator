63941af131d5e17b5456b64a311b07a1b133daa0
Cynthia Mumbua Muinde <cynmumbua@gmail.com>
2020-04-08 18:27:40 +0300

    (feature) Added hospital beds by requested time computation

--- src/estimator.js
+++ src/estimator.js
@@ -2,13 +2,15 @@ const estimatorF = (data) => {
   const input = data;
   const impact = {};
   const severeImpact = {};
-
   const ici = input.reportedCases * 10;
   impact.currentlyInfected = ici;
   impact.infectionsByRequestedTime = Math.floor(ici * (2 ** (input.timeToElapse / 3)));
   const svi = input.reportedCases * 50;
   severeImpact.currentlyInfected = svi;
   severeImpact.infectionsByRequestedTime = Math.floor(svi * (2 ** (input.timeToElapse / 3)));
+  const severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.15;
+  const bedAvailability = input.totalHospitalBeds * 0.35;
+  severeImpact.hospitalBedsByRequestedTime = bedAvailability - severeCasesByRequestedTime;
   return {
     data,
     impact,
