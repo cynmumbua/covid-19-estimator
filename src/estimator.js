37bc7267763e22af6536cba6a9ebbc571e4c8fa1
Cynthia Mumbua Muinde <cynmumbua@gmail.com>
2020-04-08 14:47:03 +0300

    (feature) Added currently infected property Added infectionsByRequestedTime

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,22 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  if (input.periodType === 'months') {
+    input.timeToElapse *= 30;
+  }
+  if (input.periodType === 'weeks') {
+    input.timeToElapse *= 7;
+  }
+  return {
+    data: input,
+    impact: {
+      currentlyInfected: input.reportedCases * 10,
+      infectionsByRequestedTime: Math.floor(currentlyInfected * (2 ** input.timeToElapse / 3))
+    },
+    severeImpact: {
+      currentlyInfected: input.reportedCases * 50,
+      infectionsByRequestedTime: Math.floor(currentlyInfected * (2 ** input.timeToElapse / 3))
+    }
+  };
+};
 
 export default covid19ImpactEstimator;
