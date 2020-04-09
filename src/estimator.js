101624545f94bbfada7914f42582ec1b0de95edd
Cynthia Mumbua Muinde <cynmumbua@gmail.com>
2020-04-09 11:10:36 +0300

    (feature) Added available hospital beds computation

--- src/estimator.js
+++ src/estimator.js
@@ -9,13 +9,13 @@ const estimatorF = (data) => {
   severeImpact.currentlyInfected = svi;
   severeImpact.infectionsByRequestedTime = svi * (2 ** Math.floor((input.timeToElapse / 3)));
 
-  const bedAvailability = input.totalHospitalBeds * 0.35;
+  const bedAvailability = Math.floor(input.totalHospitalBeds * 0.35);
 
-  impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
+  impact.severeCasesByRequestedTime = Math.floor(impact.infectionsByRequestedTime * 0.15);
   const irbt = impact.severeCasesByRequestedTime;
   impact.hospitalBedsByRequestedTime = Math.floor(bedAvailability - irbt);
 
-  const severeCasesComputation = severeImpact.infectionsByRequestedTime * 0.15;
+  const severeCasesComputation = Math.floor(severeImpact.infectionsByRequestedTime * 0.15);
   severeImpact.severeCasesByRequestedTime = severeCasesComputation;
   const sibrt = severeImpact.severeCasesByRequestedTime;
   severeImpact.hospitalBedsByRequestedTime = Math.floor(bedAvailability - sibrt);
