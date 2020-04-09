9d49baf8f30fa4e10f94419407c33dc9e3fb047e
Cynthia Mumbua Muinde <cynmumbua@gmail.com>
2020-04-09 10:13:40 +0300

    (bug) Change computaion logic

--- src/estimator.js
+++ src/estimator.js
@@ -4,10 +4,10 @@ const estimatorF = (data) => {
   const severeImpact = {};
   const ici = input.reportedCases * 10;
   impact.currentlyInfected = ici;
-  impact.infectionsByRequestedTime = ici * Math.floor((2 ** (input.timeToElapse / 3)));
+  impact.infectionsByRequestedTime = ici * (2 ** Math.floor((input.timeToElapse / 3)));
   const svi = input.reportedCases * 50;
   severeImpact.currentlyInfected = svi;
-  severeImpact.infectionsByRequestedTime = svi * Math.floor((2 ** (input.timeToElapse / 3)));
+  severeImpact.infectionsByRequestedTime = svi * (2 ** Math.floor((input.timeToElapse / 3)));
   const severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.15;
   const bedAvailability = input.totalHospitalBeds * 0.35;
   severeImpact.hospitalBedsByRequestedTime = bedAvailability - severeCasesByRequestedTime;
