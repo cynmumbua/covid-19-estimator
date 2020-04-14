fcc0bda1f94806e26b0b0d6c708d9139141f33b8
Cynthia Mumbua Muinde <cynmumbua@gmail.com>
2020-04-14 23:42:29 +0300

    (chore) Fix lint errors

--- src/estimator.js
+++ src/estimator.js
@@ -17,7 +17,7 @@ mongoose.Promise = global.Promise;
 // Connecting to the database
 mongoose.connect(dbConfig.url, {
   useNewUrlParser: true
-})
+});
 // .then(() => {
 //   // console.log('Successfully connected to the database');
 // }).catch(() => {
